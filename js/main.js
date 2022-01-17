cb4efcf6eca49f42f8fff30c190cff82b9688683
Nabeel Yousaf <192400010@gift.edu.pk>
2022-01-17 18:50:41 +0500

    cross button removed and hide overflow-y

--- js/main.js
+++ js/main.js
@@ -7,13 +7,9 @@ window.onload = function () {
         if (resBtnpressed) {
             resMenu.style.left = "-780px"
             resBtnpressed = false
-            resBtn.innerHTML = `<div class="line-1">&nbsp;</div>
-            <div class="line-2">&nbsp;</div>
-            <div class="line-3">&nbsp;</div>`
         } else {
             resMenu.style.left = "0"
             resBtnpressed = true
-            resBtn.innerHTML = `<div class="cross">+</div>`
         }
 
     })
